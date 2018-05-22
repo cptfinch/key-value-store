165db709b10c760c3dce82afa5f3ae369fccbb7d
cptfinch <cptfinch@gmail.com>
2018-05-22 12:43:41 +0100

    1 module, spawning a function as server, server uses process dictionary

--- kvs1.erl
+++ kvs1.erl
@@ -1,8 +1,26 @@
 -module(kvs1).
-% -export([start/0, store/2, lookup/1]).
--export([start/0]).
+-export([start/0, store/2, lookup/1]).
 
-start() -> register(kvs1, spawn(
-  fun() -> io:fwrite("hello there") end)
-  ).
-% start() -> register(kvs1, spawn(fun() -> loop() end)).
+start() -> register(kvs1, spawn(fun() -> loop() end)).
+
+store(Key, Value) -> rpc({store, Key, Value}).
+
+lookup(Key) -> rpc({lookup, Key}).
+
+rpc(Q) ->
+  kvs1 ! {self(), Q},
+  receive
+    {kvs1, Reply} ->
+      Reply
+  end.
+
+loop() ->
+  receive
+    {From, {store, Key, Value}} ->
+      put(Key, {ok, Value}),
+      From ! {kvs1, true},
+      loop();
+    {From, {lookup, Key}} ->
+      From ! {kvs1, get(Key)},
+      loop()
+  end.
